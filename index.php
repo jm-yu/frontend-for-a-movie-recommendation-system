cdb663557de2a473c59b0aa00a39b7002240df91
jmyu1994 <jmyu1994@gmail.com>
2018-01-11 20:47:48 -0500

    reorganized login page
    add gif and replace buttons

--- index.php
+++ index.php
@@ -11,20 +11,27 @@
     <link rel="stylesheet" href="css/login.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/login_script.js"></script>
-
 </head>
-<body>
+<body background-image="https://giphy.com/gifs/VxbP9tLeKzazm/html5">
 <!-- Button to open the modal login form -->
-<h2>Movie recommendation</h2>
-<span align="left">
+<h1 align="center">Recommend Movie for You!</h2>
+<div class = "main_container">
+  <div class = "button_container">
+<span class="left">
 <button class = "button1" onclick="document.getElementById('id01').style.display='block'">Login</button>
 </span>
-<span align="right">
+</div>
+<div class = "button_container">
+<span class="middle">
 <button class = "button1" onclick="document.getElementById('id02').style.display='block'">Sign up</button>
 </span>
-<span align="right">
+</div>
+<div class = "button_container">
+<span class="right">
 <button class = "button1" onclick="location.href='user_input.php'">Login as one-time user</button>
 </span>
+</div>
+</div>
 <!-- The Modal -->
 <div id="id01" class="modal">
   <span onclick="document.getElementById('id01').style.display='none'" 
@@ -62,7 +69,7 @@ class="close" title="Close Modal">&times;</span>
 
       <label><b>Email</b></label>
       <input id = "s_email" type="text" placeholder="email" name="s_email" required>
-      <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+      <p>By creating an account you agree to our <a href="./terms.txt">Terms & Privacy</a>.</p>
       <div id="message_2"></div>
       <div class="clearfix">
         <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
@@ -70,6 +77,12 @@ class="close" title="Close Modal">&times;</span>
       </div>
     </div>
   </form>
+</div>
+<div style="position: relative;background-color: blue; width: 100%; height:3%;">    
+
+    <div style="position: absolute; bottom: 0px; background-color: Transparent;">
+    <p>Source code for this website can be found here <a href="https://github.com/jmyu1994/frontend-for-a-movie-recommendation-system">here</a>.</p>
+    </div>
 </div>
 </body>
 </html>
